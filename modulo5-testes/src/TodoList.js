e033863f53d62af0257695853cc29fc1b00e83c7
paulocsg <pauloc.npi@gmail.com>
2019-06-21 21:29:40 -0300

    mock test, acessar state

---
+++ modulo5-testes/src/TodoList.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+
+// import { Container } from './styles';
+
+export default class src extends Component {
+  state = {
+    newTodo: "",
+    todos: []
+  };
+
+  componentDidMount() {
+    const todos = localStorage.getItem("todos");
+
+    if (todos) {
+      this.setState({ todos: JSON.parse(todos) });
+    }
+  }
+
+  handleInputChange = e => {
+    this.setState({ newTodo: e.target.value });
+  };
+
+  handleAddTodo = () => {
+    this.setState(
+      {
+        todos: [...this.state.todos, this.state.newTodo],
+        newTodo: ""
+      },
+      () => {
+        localStorage.setItem("todos", JSON.stringify(this.state.todos));
+      }
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.todos.map(todo => (
+            <li key={todo}>{todo}</li>
+          ))}
+        </ul>
+        <input
+          type="text"
+          name="todo"
+          onChange={this.handleInputChange}
+          value={this.state.newTodo}
+        />
+        <button onClick={this.handleAddTodo}>add toto</button>
+      </div>
+    );
+  }
+}
---
+++ modulo5-testes/src/__tests__/components/TodoList.spec.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { mount } from "enzyme";
+
+import TodoList from "../../TodoList";
+
+it("should render the list and button", () => {
+  const wrapper = mount(<TodoList />);
+
+  expect(wrapper.find("ul").exists()).toBe(true);
+  expect(wrapper.find("input[name='todo']").exists()).toBe(true);
+  expect(wrapper.find("button").exists()).toBe(true);
+});
+
+it("should be able to add new todo", () => {
+  const wrapper = mount(<TodoList />);
+
+  wrapper.find("input[name='todo']").simulate("change", {
+    target: { value: "Novo todo" }
+  });
+
+  wrapper.find("button").simulate("click");
+
+  expect(wrapper.find("ul").contains(<li>Novo todo</li>)).toBe(true);
+});
+
+it("should add new todos to local storage", () => {
+  const setItemMock = jest.fn();
+
+  global.localStorage.__proto__.getItem = jest.fn().mockReturnValue("[]");
+  global.localStorage.__proto__.setItem = setItemMock;
+
+  const wrapper = mount(<TodoList />);
+
+  wrapper.setState({ newTodo: "Novo todo" });
+  wrapper.instance().handleAddTodo();
+
+  expect(localStorage.setItem).toHaveBeenLastCalledWith(
+    "todos",
+    JSON.stringify(["Novo todo"])
+  );
+});
+
+it("should load todos in componentDidmount", () => {
+  const getItemMock = jest.fn().mockReturnValue(JSON.stringify(["Fazer café"]));
+
+  global.localStorage.__proto__.getItem = getItemMock;
+
+  const wrapper = mount(<TodoList />);
+
+  expect(wrapper.state("todos")).toEqual(["Fazer café"]);
+});
